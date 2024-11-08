e61169d495a04c41f576c982b27c23651303b14d
jp-96 <jp-96@outlook.jp>
2024-11-08 09:56:23 +0000

    QuatEstimate

---
+++ src/components/HeartRateContextProvider.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { createServiceBuilder, DeviceInformationService } from '@weblueth/gattbuilder';
+import { WbxContextProvider } from '@weblueth/react';
+import { HeartRateService } from '../services/HeartRateService'
+
+const requestHeartRateSensor = async (bluetooth: Bluetooth): Promise<BluetoothDevice | undefined> => {
+    return await bluetooth.requestDevice({
+        //filters: [{ services: ['heart_rate'] }],
+        // optionalServices: ['battery_service', 'device_information']
+        //filters: [{ namePrefix: 'BBC micro:bit', services: [HeartRateService.uuid] }],
+        //filters: [{ services: [HeartRateService.uuid] }],
+        filters: [{ namePrefix: 'BBC micro:bit'}],
+        optionalServices: ['device_information', HeartRateService.uuid]
+    });
+};
+
+export type Services = {
+    heartRateService?: HeartRateService;
+    // battery_service?: BluetoothRemoteGATTService;
+    deviceInformationService?: DeviceInformationService
+}
+
+const retrieveServices = async (device: BluetoothDevice): Promise<Services> => {
+    if (!device || !device.gatt) {
+        return {};
+    }
+
+    if (!device.gatt.connected) {
+        await device.gatt.connect();
+    }
+    const services = await device.gatt.getPrimaryServices();
+    const builder = createServiceBuilder(services);
+    const heartRateService = await builder.createService(HeartRateService);
+    // const battery_service = await device.gatt.getPrimaryService('battery_service');
+    const deviceInformationService = await builder.createService(DeviceInformationService);
+    // return { heartRateService, battery_service, deviceInformationService };
+    return { heartRateService, deviceInformationService };
+};
+
+type Props = {
+    children: any;
+    bluetooth?: Bluetooth;
+    connectionName?: string;
+}
+
+export function HeartRateContextProvider(props: Props) {
+    const connectionName = props.connectionName ?? "Heart Rate";
+    return (
+        <WbxContextProvider
+            retrieveServices={retrieveServices} requestDevice={requestHeartRateSensor}
+            bluetooth={props.bluetooth} connectionName={connectionName}>
+            {props.children}
+        </WbxContextProvider>
+    );
+}
